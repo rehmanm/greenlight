c7967898590a50a9e9b61c49c613aef6e4f8be5e
rehmanm <muhammad.abdulrehman@gmail.com>
2023-08-23 16:23:54 -0400

    filters, pagination done

---
+++ internal/data/filters.go
@@ -0,0 +1,49 @@
+package data
+
+import (
+	"strings"
+
+	"github.com/rehmanm/greenlight/internal/validator"
+)
+
+type Filters struct {
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafeList []string
+}
+
+func ValidateFilters(v *validator.Validator, filters Filters) {
+
+	v.Check(filters.Page > 0, "page", "must be greater than zero")
+	v.Check(filters.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+	v.Check(filters.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(filters.PageSize <= 100, "page_size", "must be maximum of 100")
+
+	v.Check(validator.PermittedValue(filters.Sort, filters.SortSafeList...), "sort", "invalid sort value")
+
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameters" + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}

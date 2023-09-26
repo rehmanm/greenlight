d509319728b66358737a8c778315d8e41e4b9ca3
rehmanm <muhammad.abdulrehman@gmail.com>
2023-09-25 22:39:37 -0400

    count by status and url

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -211,22 +212,61 @@ func (app *application) enableCors(next http.Handler) http.Handler {
 	})
 }
 
+type metricsResponseWriter struct {
+	wrapped       http.ResponseWriter
+	statusCode    int
+	headerWritten bool
+}
+
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped:    w,
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
+func (mw *metricsResponseWriter) UnWrap() http.ResponseWriter {
+	return mw.wrapped
+}
+
 func (app *application) metrics(next http.Handler) http.Handler {
 	var (
 		totalRequestReceived            = expvar.NewInt("total_request_received")
 		totalResponsesSent              = expvar.NewInt("total_responses_sent")
 		totalProcessingTimeMicroSeconds = expvar.NewInt("total_processing_time_μs")
+		totalResponsesSentByStatus      = expvar.NewMap("total_responses_sent_by_status")
+		totalRequestByUrl               = expvar.NewMap("total_request_by_url")
 	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		start := time.Now()
 		totalRequestReceived.Add(1)
-		next.ServeHTTP(w, r)
+		mw := newMetricsResponseWriter(w)
+		next.ServeHTTP(mw, r)
 		totalResponsesSent.Add(1)
 
 		duration := time.Since(start).Microseconds()
-
+		totalResponsesSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
+		totalRequestByUrl.Add(r.URL.Path, 1)
 		totalProcessingTimeMicroSeconds.Add(duration)
 
 	})
